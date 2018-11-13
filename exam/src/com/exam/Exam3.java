c0eeb602d97963404c96b0e68c4e67dc828531e5
winnie <winnie688801@gmail.com>
2018-11-13 11:58:35 +0800

    teach

--- exam/src/com/exam/Exam3.java
+++ exam/src/com/exam/Exam3.java
@@ -4,48 +4,48 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Exam3 {
-    /* 第三題  猜數字程式 20分 
-		1. 先產生一個介於1到10的亂數，為祕密數字 (假設是7)
-		2. 讓使用者猜這個數字，如下:
-		Your guess: 5(使用者猜5)
-		3. 提示使用者要高一點或低一點，再請他猜下一次，如下:
+    /* 蝚砌���  ��摮��� 20��� 
+		1. �������1�10���嚗蟡�摮� (��身�7)
+		2. 霈蝙�����摮����:
+		Your guess: 5(雿輻���5)
+		3. ��內雿輻����暺���暺�������甈∴����:
 		higher
-		Your guess: 9(使用者猜9)
+		Your guess: 9(雿輻���9)
 		lower
-		4. 當使用者猜中祕密數字時，印出:
+		4. �雿輻��葉蟡�摮���:
 		Great! The secret number is 7
 		
-		提示: 在while迴圈中可使用break指令跳出迴圈外。
-		● 若有利用類別設計完成此題，加倍給分。
+		��內: �while餈游�葉�雿輻break��誘頝喳餈游����
+		��� ����憿閮剛���迨憿���策����
 		
-	    第四題 計算猜幾次 10分
-	        承第三題，如果使用者猜二次以內(包括二次)猜對時，請顯示:
+	    蝚砍��� 閮��嗾甈� 10���
+	        �蝚砌�����蝙����活隞亙(��鈭活)������＊蝷�:
 	   Excellent! The secret number is 7
 
     */
 	public static void main(String[] args) {
-		Random random = new Random();
-		int secret = random.nextInt(10)+1;
+		Secret secret = new Secret();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println(secret);
+		int diff = -1;
+		System.out.println();
+
 		
-		while(true){
-			Secret s = new Secret();
-			s.first();
-			int i = scanner.nextInt();
-			
-			if(i>secret){
-				System.out.println("Your guess : " + i);
-				s.second();
+		while(diff != 0 ){
+			System.out.println("Please guess a number:");
+			int number = scanner.nextInt();
+			diff = secret.guess(number);
+			if(diff>0){
+				System.out.println("Your guess : " + number);
+				System.out.println("Higher");
 			}
 			
-			else if(i<secret){
-				System.out.println("Your guess : " + i);
-				s.third();
+			else if(diff<0){
+				System.out.println("Your guess : " + number);
+				System.out.println("Lower");
 			}
 			
-			else if(i==secret){
-					System.out.println("Great! The secret number is " + i);
+			else if(diff==0){
+					System.out.println("Great! The secret number is " + secret.secret);
 				break;
 			}
 		}
--- exam/src/com/exam/Secret.java
+++ exam/src/com/exam/Secret.java
@@ -1,6 +1,18 @@
 package com.exam;
 
+import java.util.Random;
+
 public class Secret {
+	int secret;
+	public Secret(){
+		Random random = new Random();	
+		secret = random.nextInt(10)+1;
+		System.out.println(secret);
+	}
+	public int guess(int number){
+		return secret - number;
+	}
+	
 	public void first(){
 		System.out.println("Please guess a number:");
 	}
